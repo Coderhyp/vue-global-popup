5a0e9dd44f7c2186a6f09bb699fce82236bf584e
coderhyp <slimshadyedc@gmail.com>
2025-07-29 20:07:02 +0800

    fix: use relative path for popup-registry import in Popup.vue

---
+++ graph-vue3-demo/src/views/Popup.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="openDialog">打开弹窗</button>
+</template>
+<script setup lang="ts">
+import { useUserFormDialog } from "../business-popups/popup-registry";
+const { show } = useUserFormDialog();
+const openDialog = () => {
+  show();
+};
+</script>
